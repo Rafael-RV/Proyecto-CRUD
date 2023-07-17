5901ff988da6ae8f0f4b747f954922582e2b09a1
Rafael-RV <desarrollador20223@gmail.com>
2023-07-17 16:50:25 -0400

    script

---
+++ app.js
@@ -0,0 +1,103 @@
+// Usuario
+class Paciente {
+  constructor(NombresYApellidos, Edad, MotivosDeLaConsulta) {
+    this.NombresYApellidos = NombresYApellidos;
+    this.Edad = Edad;
+    this.MotivosDeLaConsulta = MotivosDeLaConsulta;
+  }
+}
+
+ // Funcion para mostrar los pacientes en la lista
+function showData() {
+  let patientsList;
+
+  if (localStorage.getItem("patientsList") === null) {
+    patientsList = [];
+  } else {
+    patientsList = JSON.parse(localStorage.getItem("patientsList"))
+  }
+
+  let html = "";
+  patientsList.forEach((patient, index) => {
+    html += `<td>${patient.NombresYApellidos} ${patient.Edad} ${patient.MotivosDeLaConsulta}<button onclick="deleteData(${index})">Eliminar</button><button onclick="editData(${index})">Editar</button></td>`;
+  });
+
+  document.querySelector('tbody').innerHTML = html;
+}
+
+// Evento onload para mostrar los pacientes cuando se carga la página
+document.onload = showData();
+
+// Función para agregar un nuevo paciente
+function addData(event) {
+  event.preventDefault();
+  let NombresYApellidos = document.querySelector('#input-nombre').value;
+  let Edad = document.querySelector('#input-edad').value;
+  let MotivosDeLaConsulta = document.querySelector('#input-motivo').value;
+
+  if (NombresYApellidos === "" || Edad === "" || MotivosDeLaConsulta === "") {
+    alert("Por favor, complete todos los campos.");
+    return;
+  }
+
+  const paciente = new Paciente(NombresYApellidos, Edad, MotivosDeLaConsulta);
+  let patientsList;
+  if (localStorage.getItem("patientsList") === null) {
+    patientsList = [];
+  } else {
+    patientsList = JSON.parse(localStorage.getItem("patientsList"));
+  }
+  patientsList.push(paciente);
+  localStorage.setItem("patientsList", JSON.stringify(patientsList));
+  showData();
+
+  document.querySelector('#input-nombre').value = "";
+  document.querySelector('#input-edad').value = "";
+  document.querySelector('#input-motivo').value = "";
+}
+
+// Función para editar los datos de un paciente
+function editData(index) {
+  document.getElementById('add-btn').style.display = 'none';
+  document.getElementById('edit-btn').style.display = 'block';
+
+  let patientsList;
+  if (localStorage.getItem("patientsList") === null) {
+    patientsList = [];
+  } else {
+    patientsList = JSON.parse(localStorage.getItem("patientsList"));
+  }
+
+  document.querySelector('#input-nombre').value = patientsList[index].NombresYApellidos;
+  document.querySelector('#input-edad').value = patientsList[index].Edad;
+  document.querySelector('#input-motivo').value = patientsList[index].MotivosDeLaConsulta;
+
+  document.getElementById('edit-btn').onclick = function () {
+    patientsList[index].NombresYApellidos = document.querySelector('#input-nombre').value;
+    patientsList[index].Edad = document.querySelector('#input-edad').value;
+    patientsList[index].MotivosDeLaConsulta = document.querySelector('#input-motivo').value;
+
+    localStorage.setItem("patientsList", JSON.stringify(patientsList));
+    showData();
+    document.querySelector('#input-nombre').value = "";
+    document.querySelector('#input-edad').value = "";
+    document.querySelector('#input-motivo').value = "";
+
+    document.getElementById('add-btn').style.display = 'block';
+    document.getElementById('edit-btn').style.display = 'none';
+  }
+}
+
+// Función para eliminar un paciente
+function deleteData(index) {
+  let patientsList;
+  if (localStorage.getItem("patientsList") === null) {
+    patientsList = [];
+  } else {
+    patientsList = JSON.parse(localStorage.getItem("patientsList"));
+  }
+
+  patientsList.splice(index, 1);
+  localStorage.setItem("patientsList", JSON.stringify(patientsList));
+  showData();
+}
